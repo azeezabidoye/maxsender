8b56555637c89e7c6ec5c041215973ae442657e8
azeezabidoye <azeezabidoye@yahoo.com>
2024-10-11 11:41:11 +0100

    Styles updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,13 +23,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-          <link rel="preconnect" href="https://fonts.googleapis.com">
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
       </head>
-      <body>
-        {children}
-      </body>
+
+      <body>{children}</body>
     </html>
   );
 }
