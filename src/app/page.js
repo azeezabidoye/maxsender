53878f5b29b3a88e8c73c58a1810c2ec1f785d55
azeezabidoye <azeezabidoye@yahoo.com>
2024-10-14 11:10:38 +0100

    Components updated

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,21 @@
 
 export default function Home() {
   return (
-    // code here
-    <>Maxsend</>
+    <>
+      <div className="container-fluid mt-5 d-flex justify-content-center">
+        <div id="content" className="row">
+          <div id="content-inner" className="col">
+            <div className="text-center">
+              <h1 id="title" className="fw-bold">
+                MAXSENDER
+              </h1>
+              <p id="sub-title" className="mt-4 fw-bold">
+                Send many payments <span>in just one transaction</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
